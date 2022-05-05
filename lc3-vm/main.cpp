8ae1a179419a934a8ab913eeb5f045e3167550c1
dcb9 <bob@phpor.me>
2022-05-05 16:44:01 +0800

    Initial Commit

---
+++ lc3-vm/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lc3-vm
+//
+//  Created by Bob on 2022/5/5.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
